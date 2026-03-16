61bce2f09ff0f49d62be356d56fd6fddcc67dac1
Alexander Schneider <alexander@adanos.org>
2026-03-16 08:18:45 +0100

    Add adanos-cli formula

---
+++ Formula/adanos-cli.rb
@@ -0,0 +1,28 @@
+class AdanosCli < Formula
+  desc "Comprehensive CLI for the Adanos Finance Sentiment API"
+  homepage "https://adanos.org"
+  version "1.20.2"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/adanos-software/adanos-cli/releases/download/v1.20.2/adanos-cli-1.20.2-darwin-arm64.tar.gz"
+      sha256 "14e89986f220b1a8abf748429d5806aab453c5fc9fb278c83f94408013db47fa"
+    else
+      url "https://github.com/adanos-software/adanos-cli/releases/download/v1.20.2/adanos-cli-1.20.2-darwin-x86_64.tar.gz"
+      sha256 "6423101b9e8d1ca416872a3bb49596c3c230fbcee2fe5f4b27d2d178e1a1c3db"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/adanos-software/adanos-cli/releases/download/v1.20.2/adanos-cli-1.20.2-linux-x86_64.tar.gz"
+    sha256 "3a959e29a285fda48e523898b51851b5e4b200d96f436f4779277eb46cf57ce3"
+  end
+
+  def install
+    bin.install "adanos"
+  end
+
+  test do
+    assert_match "adanos", shell_output("#<built-in function bin>/adanos --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+Homebrew tap for Adanos Software packages.
+
+Install the Adanos CLI:
+
+```bash
+brew install adanos-software/tap/adanos-cli
+```
